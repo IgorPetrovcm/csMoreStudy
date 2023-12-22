8eeeb19bdd7050fb1ab40303a853f32713b1140c
Honor <igorpetrov42ru@gmail.com>
2023-12-22 13:06:14 +0300

    Read csv file using ScvHelper lib

---
+++ WorkWith_/csvFiles/ReaderCsvFile/Task1/Employee.cs
@@ -0,0 +1,10 @@
+namespace Task1;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Last_Name { get; set; }
+    public string First_Name { get; set; }
+    public int Age { get; set; }
+    
+}
---
+++ WorkWith_/csvFiles/ReaderCsvFile/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Task1;
+
+using CsvHelper;
+
+class Program
+{
+    static void Main()
+    {
+        using (StreamReader reader = new StreamReader(@"../../../employees.csv"))
+        {
+            using (CsvReader csvReader = new CsvReader(reader,CultureInfo.InvariantCulture))
+            {
+                IEnumerable<Employee> employees = csvReader.GetRecords<Employee>();
+                foreach (Employee employee in employees)
+                {
+                    Console.WriteLine($"{employee.Id} - {employee.Last_Name} - {employee.First_Name} - {employee.Age}");
+                }
+            }
+        }
+    }
+}
