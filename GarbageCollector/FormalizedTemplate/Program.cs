38e69069cc6ae210bb44b04c037614b5053813bd
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-15 23:54:00 +0300

    Шаблон реализации дуструктора и интерфейса IDisposable

---
+++ GarbageCollector/FormalizedTemplate/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace FormalizedTemplate;
+
+
+public class Program
+{
+    static void Main()
+    {
+
+    }
+}
+
+public class MyClass : IDisposable
+{
+    private bool disposed = false;
+
+    //Реализация IDisposable
+    public void Dispose()
+    {
+        //Освободить не управляемые ресурсы 
+        Dispose(true);
+
+        //Блокируем вызов Finalize
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed) return;
+
+        if (disposing)
+        {
+            //Осовобождение управляемых ресурсов
+        }
+        //освобождение неуправляемых ресурсов 
+        disposed = true;
+    }
+
+    //Деструктор
+    ~MyClass()
+    {
+        Dispose(false);
+    }
+}
