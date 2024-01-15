6fce6ed8271ecd8a8ebe1c040c9907e02d12cc2f
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-16 00:29:09 +0300

    unsafe and pointers

---
+++ WorkWith_/Pointers/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Pointers_Unsafe;
+
+
+public class Pointers
+{
+    static void Main()
+    {
+        unsafe{
+            /*int* a;
+
+            int y = 20;
+
+            a = &y;
+
+            System.Console.WriteLine(*a);
+
+            y = 50;
+
+            System.Console.WriteLine(*a);
+
+            *a = 30;
+
+            System.Console.WriteLine(y);*/
+
+            Data data = new Data(5,10);
+
+            Data* pointToData = &data;
+
+            //Мф не можем на прямую обратиться к полям структуры, так как pintToData это лишь указатель на структуру, поэтому используем конструкцию '->'
+            pointToData->X = 20;
+
+            System.Console.WriteLine(*pointToData);
+
+            System.Console.WriteLine(pointToData -> X);
+
+            //Также есть альтернатива '->' 
+            (*pointToData).Y = 100;
+
+            System.Console.WriteLine((*pointToData).Y);
+
+        }
+    }
+    struct Data
+    {
+        public int X {get;set;}
+
+        public int Y {get;set;}
+
+        public Data(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{X} - {Y}";
+        }
+    }
+}
---
+++ WorkWith_/Pointers/README.md
@@ -0,0 +1,2 @@
+## Включить 'Небезопасный код' в vscode
+в **.csproj** файл проекта, в `<PropertyGroup>` вставим строку `<AllowUnsafeBlocks>true</AllowUnsafeBlocks>`
