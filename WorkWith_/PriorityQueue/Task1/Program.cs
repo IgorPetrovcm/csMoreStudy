4a009bef9d8d7078733fcf9224a9c922b02b231a
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-16 15:52:15 +0300

    Priority Queue

---
+++ WorkWith_/PriorityQueue/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1;
+
+
+public class Program
+{
+    static void Main()
+    {
+        PriorityQueue<int,int> queue = new PriorityQueue<int, int>();
+
+        queue.Enqueue(1,1);
+        queue.Enqueue(2,2);
+        queue.Enqueue(3,3);
+        queue.Enqueue(4,4);
+        queue.Enqueue(5,5);
+        queue.Enqueue(6,6);
+
+        //Удаляем низший по приоритету элемент
+        System.Console.WriteLine(queue.Dequeue());
+
+        //Возвращаем низший по приоритету элемент
+        System.Console.WriteLine(queue.Peek());
+
+        
+    }
+}
