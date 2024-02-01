130b5d6b5bcd0585d4040956ff51476e77cba037
Honor <igorpetrov42ru@gmail.com>
2024-02-01 23:07:51 +0300

    Практика yield, yield классная вещь

---
+++ WorkWith_/Yield/Task/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task;
+
+
+public class Program
+{
+    static void Main()
+    {
+        IEnumerable<string> inputs = GetUsersInput();
+
+        foreach (string line in inputs)
+        {
+            System.Console.WriteLine("Вы ввели: " + line);
+        }
+    }
+    static IEnumerable<string> GetUsersInput()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == null || input.Trim().Length == 0)
+                yield break;
+            
+            if (input == "exit")
+                break;
+
+            yield return input;
+        }
+    }
+}
+
+public static class UserInputForm
+{
+
+}
