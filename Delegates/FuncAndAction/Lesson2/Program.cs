b0cb2354f98abbca5c4ab4287934327a6383e87f
Honor <igorpetrov42ru@gmail.com>
2024-02-01 16:12:51 +0300

    Поработал с Func, Lesson2 это шаблон для работы с ним, делегаты крутая штука

---
+++ Delegates/FuncAndAction/Lesson2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson2;
+
+
+public class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine(YourOperation("hell",x => x + x));
+    }
+
+    public static string YourOperation(string line, Func<string, string> func)
+    {
+        return func(line);
+    }
+}
