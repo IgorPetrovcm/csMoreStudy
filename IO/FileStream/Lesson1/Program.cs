41e2820b802b09589f260cdfa70001f0332b460d
Игорь Петров <igorpetrov42ru@gmail.com>
2024-02-05 17:45:14 +0300

    FileStream or bytes

---
+++ IO/FileStream/Lesson1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lesson1;
+
+
+public class Program 
+{
+    static async Task Main()
+    {
+        byte[] buf1 = {23,55,123,12,33};
+
+        FileStream file = new FileStream("text.txt",FileMode.Create, FileAccess.Write);
+        
+        file.Write(buf1,0,buf1.Length);
+
+        file.Close();
+
+        byte[] buf2;
+
+        FileStream file2 = new FileStream("text.txt",FileMode.Open, FileAccess.Read);
+
+        buf2 = new byte[file2.Length];
+
+        file2.Read(buf2,0,(int)file2.Length);
+
+        file2.Close();
+
+        for (int i = 0; i < buf2.Length; i++)
+        {
+            System.Console.WriteLine(buf2[i]);
+        }
+    }
+}
