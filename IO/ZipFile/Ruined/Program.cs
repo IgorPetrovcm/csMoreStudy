afe5d037a651027501416c2a0b7c92996c25fd0e
Игорь Петров <igorpetrov42ru@gmail.com>
2024-02-05 22:42:48 +0300

    Разорхивая и архивация файлов

---
+++ IO/ZipFile/Ruined/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ruined;
+
+using System.IO.Compression;
+
+public class Program 
+{
+    static void Main()
+    {
+        //Разорхивация
+        //ZipFile.ExtractToDirectory("48.zip","extract/");
+
+        //Архивация
+        ZipFile.CreateFromDirectory("extract","newZip.zip");
+    }
+}
