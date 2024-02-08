5280f03d4b8a0d25d5b98131f2d1507aeb8cd0bf
Игорь Петров <igorpetrov42ru@gmail.com>
2024-02-08 23:22:47 +0300

    work with json

---
+++ IO/Json/Lesson1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lesson1;
+
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+public class Program
+{
+    static void Main()
+    {
+        using (FileStream fs = new FileStream( "my.json", FileMode.OpenOrCreate))
+        {
+            List<string> list = new List<string>()
+            {
+                "one",
+                "two",
+                "three",
+                "four"
+            };
+
+            JsonSerializer.Serialize<List<string>>(fs, list);
+        }
+
+        using (FileStream fs = new FileStream( "my.json", FileMode.OpenOrCreate ))
+        {
+            List<string> list = JsonSerializer.Deserialize<List<string>>(fs);
+
+            foreach (string line in list)
+            {
+                System.Console.WriteLine(line);
+            }
+        }
+    }
+}
