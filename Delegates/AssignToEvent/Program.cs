66288c1e7525c5970129b5c317d82f40ffd4c92b
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-02-23 23:28:34 +0300

    Assign to event

---
+++ Delegates/AssignToEvent/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace AssigntoEvent;
+
+
+public class Program 
+{
+    static void Main()
+    {
+        TestClass testClass = new TestClass();
+
+        testClass.operationEvent += (x) => x + x;
+
+        testClass.operationEvent += (x) => x * x;
+
+        Console.WriteLine(testClass.PrintOperation(5));
+    }
+}
+
+public class TestClass
+{
+
+    public event Func<double, double> operationEvent;
+
+    public void SetOperation(Func<double, double> func)
+    {
+        operationEvent = func;
+    }
+
+    public void AddOperation(Func<double, double> func)
+    {
+        operationEvent += func;
+    }
+
+    public double PrintOperation(int a)
+    {
+        return operationEvent.Invoke(a);
+    }
+}
