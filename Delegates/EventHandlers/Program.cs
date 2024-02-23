4b17d81ffed426bdb7c4d8a2122066588335dbcc
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-02-23 21:34:11 +0300

    EventHandlers

---
+++ Delegates/EventHandlers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace EventHandlers;
+
+
+public class Program
+{
+    static void Main()
+    {
+        Counter counter = new Counter(1);
+
+        counter.ThresholdReached += EventMethodThersholdReached;
+
+        counter.Add(2);
+    }
+
+    static void EventMethodThersholdReached(object? sender, EventArgs e)
+    {
+        Counter? counter = sender as Counter;
+
+        if (counter is not null)
+        {
+            System.Console.WriteLine(counter.Total);
+        }
+    }
+}
+
+public class Counter
+{
+    private readonly int threshold;
+
+    private int total;
+
+    public int Total => total;
+
+    public Counter(int threshold)
+    {
+        this.threshold = threshold ;
+    }
+
+    public void Add(int x)
+    {
+        total += x;
+
+        if (total >= threshold)
+        {
+            ThresholdReached?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public event EventHandler ThresholdReached;
+}
