5d1ebfbf806a328685673bac2c8f53b1baa15de9
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-02-26 01:32:04 +0300

    неплохой калькулятор, но стоит все писать в xaml, так как проблемы с поиском элементов созданных в коде c#

--- MAUI/Projects/Calculator/App.xaml.cs
+++ MAUI/Projects/Calculator/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator
+﻿
+
+namespace Calculator
 {
 	public partial class App : Application
 	{
@@ -6,7 +8,20 @@
 		{
 			InitializeComponent();
 
-			MainPage = new AppShell();
+			MainPage = new MainPage();
+		}
+
+		protected override Window CreateWindow(IActivationState? activationState)
+		{
+			Window window = base.CreateWindow(activationState);
+
+			window.Height = 450;
+			window.Width = 400;
+			window.MaximumWidth = 500;
+			window.MinimumWidth = 400;
+			window.MaximumHeight = 500;
+			window.MinimumHeight = 400;
+			return window;
 		}
 	}
 }
--- MAUI/Projects/Calculator/MainPage.xaml.cs
+++ MAUI/Projects/Calculator/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace Calculator
 {
 	public partial class MainPage : ContentPage
 	{
@@ -7,19 +9,103 @@
 		public MainPage()
 		{
 			InitializeComponent();
+
+			Grid grid = new Grid()
+			{
+				Padding = new Thickness(5, 5, 5, 5)
+			};
+
+			Border border = new Border()
+			{
+				Stroke = Colors.Blue,
+				BackgroundColor = Colors.Gray,
+				StrokeShape = new RoundRectangle() { CornerRadius = 15},
+				StyleId = "mainBorder"
+			};
+
+			Grid innerInBorderGrid = new Grid()
+			{
+				RowDefinitions =
+					{
+						new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) },
+						new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) },
+						new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) },
+						new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) },
+						new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) },
+					},
+				ColumnDefinitions =
+					{
+						new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
+						new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
+						new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
+						new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) },
+					},
+
+				Padding = new Thickness(5, 5, 5, 5),
+				RowSpacing = 25
+			};
+
+			int row = 1;
+			int column = 0;
+
+			for (int i = 0; i < 9; i++)
+			{
+				if (column == 3)
+				{
+					row++;
+					column = 0;
+				}
+
+				Button button = new Button { Text = (i + 1).ToString(), StyleId = "number" + i + 1, Margin = new Thickness(5,5,5,5), FontSize=20, HeightRequest = 80,  };
+
+				button.Clicked += ButtonWithNumberClicked;
+
+				innerInBorderGrid.Add(button, column, row);
+
+				column++;
+			}
+
+			Button btnAnswer = new Button() { Text = "=", StyleId = "btn=", Margin = new Thickness(5, 5, 5, 5), FontSize = 20, HeightRequest = 80 };
+
+			Button btnDivision = new Button() { Text = "/", StyleId = "btn/", Margin = new Thickness(5, 5, 5, 5), FontSize = 20, HeightRequest = 80 };
+
+			Button btnZero = new Button() { Text = "0", StyleId = "btn0", Margin = new Thickness(5, 5, 5, 5), FontSize = 20, HeightRequest = 80 };
+
+			Entry entry = new Entry()
+			{
+				Margin = new Thickness(0,0,0,5), 
+				FontSize = 20,
+				StyleId = "inputNumbers",
+				ClearButtonVisibility = ClearButtonVisibility.WhileEditing,
+				Keyboard = Keyboard.Numeric,
+				MaxLength = 8,
+				BackgroundColor = Colors.WhiteSmoke
+			};
+			innerInBorderGrid.Add(entry, 0, 0);
+			innerInBorderGrid.SetColumnSpan(entry, 4);
+
+			innerInBorderGrid.Add(btnAnswer, 2, 4);
+			innerInBorderGrid.Add(btnDivision, 0, 4);
+			innerInBorderGrid.Add(btnZero, 1, 4);
+
+			border.Content = innerInBorderGrid;
+
+			grid.Add(border);
+
+			Content = grid;
 		}
 
-		private void OnCounterClicked(object sender, EventArgs e)
+		private void ButtonWithNumberClicked(object? sender, EventArgs e)
 		{
-			count++;
+			Button button = sender as Button;
 
-			if (count == 1)
-				CounterBtn.Text = $"Clicked {count} time";
-			else
-				CounterBtn.Text = $"Clicked {count} times";
+			Border border = this.FindByName<Border>("mainBorder");
 
-			SemanticScreenReader.Announce(CounterBtn.Text);
+			Entry entry = border.FindByName<Entry>("inputNumbers");
+
+			entry.Text += button.StyleId[StyleId.Length - 1];
 		}
+
 	}
 
 }
