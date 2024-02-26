8cca54dd252e07deaf230c9ef1226b3e86103849
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-02-27 02:15:51 +0300

    Triggers

---
+++ MAUI/Lessons/Triggers/NewPage1.xaml.cs
@@ -0,0 +1,15 @@
+namespace Triggers;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+
+
+		/*Trigger trigger = new Trigger(typeof(Entry))
+		{ 
+			Property = Len
+		}*/
+	}
+}
